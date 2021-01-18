992a99bc4821b6f396e58a4ddbe0cf91f1ca733e
dawidzebacki <dawidzebacki@protonmail.com>
2021-01-18 16:44:31 +0100

    add: global styles and fonts

---
+++ front/src/assets/styles/theme.js
@@ -0,0 +1,34 @@
+const theme = {
+  colors: {
+    bright: "#fff",
+    dark: "#000",
+    gray: "#ccc",
+	mainBg: "#F8F8F8",
+	
+    inputColor: "#99A3BA",
+    inputBorder: "#CDD9ED",
+    inputBackground: "#fff",
+    inputPlaceholder: "#CBD1DC",
+
+    inputBorderFocus: "#275EFE",
+
+    groupColor: "#99A3BA",
+    groupBorder: "#CDD9ED",
+    groupBackground: "#EEF4FF",
+
+    groupColorFocus: "#fff",
+    groupBorderFocus: "#275EFE",
+    groupBackgroundFocus: "#678EFE",
+  },
+  mq: {
+    mobileMid: "@media (min-width: 23.5rem)",
+    mobileLandscape: "@media (min-width: 30.5rem)",
+    tablet: "@media (min-width: 37.5rem)",
+    tabletMid: "@media (min-width: 42rem)",
+    tabletLandscape: "@media (min-width: 56.25rem)",
+    desktop: "@media (min-width: 75rem)",
+    desktopBig: "@media (min-width: 112.5rem)",
+  },
+};
+
+export default theme;
