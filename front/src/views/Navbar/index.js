79686dc05caad38e8cb77024edd98a6ceac02d02
dawidzebacki <dawidzebacki@protonmail.com>
2021-01-19 00:23:03 +0100

    add: navbar view

---
+++ front/src/views/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { AiOutlineHome, AiOutlineShoppingCart } from "react-icons/ai";
+
+import { Container, Wrapper, StyledLink } from "./styles";
+
+const Navbar = () => {
+  return (
+    <>
+      <Container>
+        <Wrapper>
+          <StyledLink as={Link} to="/">
+            <AiOutlineHome />
+          </StyledLink>
+          <StyledLink as={Link} to="/cart">
+            <AiOutlineShoppingCart />
+          </StyledLink>
+        </Wrapper>
+      </Container>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ front/src/views/Navbar/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	position: fixed;
+	background-color: ${({ theme }) => theme.colors.groupBackgroundFocus};
+	height: 5vh;
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	z-index: 1;
+`
+
+export const Wrapper = styled.div`
+	width: 100%;
+	max-width: 1360px;
+	margin: 0 1em;
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;	
+`;
+
+export const StyledLink = styled.a`
+	text-decoration: none;
+	color: white;
+	font-size: 2em;
+	padding: 0;
+	margin: 0.1em 0.5em 0 0.5em;
+`
