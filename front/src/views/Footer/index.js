dc97bf9edd1e4fdcb56d8fd24066e97fbe57baf0
dawidzebacki <dawidzebacki@protonmail.com>
2021-01-19 20:24:47 +0100

    add: footer data and styles

--- front/src/views/Footer/index.js
+++ front/src/views/Footer/index.js
@@ -1,16 +1,38 @@
 import React from "react";
 
-import { Container, Wrapper } from "./styles";
+import { AiFillHeart, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
-const Navbar = () => {
+import { Container, Wrapper, Text, TextSpan, Icons, Icon } from "./styles";
+
+const Footer = () => {
   return (
     <>
       <Container>
         <Wrapper>
+          <Text>
+            developed with{" "}
+            <TextSpan>
+              <AiFillHeart />
+            </TextSpan>{" "}
+            by Dawid Zębacki, {new Date().getFullYear()}
+          </Text>
+          <Icons>
+            <a href="https://github.com/dawidzebacki">
+              <Icon>
+                <AiFillGithub />
+              </Icon>
+            </a>
+
+            <a href="https://www.linkedin.com/in/dawidzebacki">
+              <Icon>
+                <AiFillLinkedin />
+              </Icon>
+            </a>
+          </Icons>
         </Wrapper>
       </Container>
     </>
   );
 };
 
-export default Navbar;
+export default Footer;
--- front/src/views/Footer/styles.js
+++ front/src/views/Footer/styles.js
@@ -1,13 +1,36 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-	background-color: ${({ theme }) => theme.colors.groupBackgroundFocus};
-	height: 5vh;
-	display: flex;
-`
-
-export const Wrapper = styled.div`
-	max-width: 1360px;
-	margin: auto;
+  background-color: ${({ theme }) => theme.colors.groupBackgroundFocus};
+  height: 5vh;
+  display: flex;
 `;
 
+export const Wrapper = styled.div`
+  max-width: 1360px;
+  display: flex;
+  margin: auto;
+`;
+
+export const Text = styled.p`
+  margin: 0;
+  padding: 0;
+  font-size: 0.9em;
+  color: white;
+`;
+
+export const TextSpan = styled.span`
+  color: red;
+  font-size: 1.1em;
+`;
+
+export const Icons = styled.div`
+	font-size: 1.1em;
+  margin: 0 0 0 2em;
+  display: flex;
+`
+
+export const Icon = styled.div`
+  margin: 0 0.25em;
+  color: black;
+`
